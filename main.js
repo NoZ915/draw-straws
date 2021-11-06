fce5d82aa7d329e90cfe3988d47dc829a50a2b3d
NoZ-design <tina0915tw0915@gmail.com>
2021-11-06 17:31:22 +0800

    second commit

---
+++ main.js
@@ -0,0 +1,8 @@
+clickToInput = function () {
+  const drawStrawsContent = $("#drawStrawsContent").val()
+  const printList = $("#printList")
+  const newLI = document.createElement("li")
+  newLI.append(drawStrawsContent)
+  printList.append(newLI)
+  document.getElementById("drawStrawsContent").value = ""
+}
